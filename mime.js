f67816884e137c5bd0f86c20fba712b7d9550c2b
John Wasterlund <john@grunka.se>
2011-02-28 23:54:07 +0100

    Making sure mime types are loaded before server starts listening

--- mime.js
+++ mime.js
@@ -5,22 +5,20 @@ var sys = require("sys");
 
 exports.init = function() {
     var lookup = {};
-    fs.readFile("mime.types", "ascii", function(err, data) {
-        if (err) throw err;
-        var lines = data.split("\n");
-        lines.forEach(function(line) {
-            if (!line.match(/^#/)) {
-                var item = line.match(/^(.*?)[\s]+(.*?)$/);
-                if (item) {
-                    var type = item[1];
-                    item[2].split(" ").forEach(function(extension) {
-                        lookup[extension] = type;
-                    });
-                }
+    var data = fs.readFileSync("mime.types", "ascii");
+    var lines = data.split("\n");
+    lines.forEach(function(line) {
+        if (!line.match(/^#/)) {
+            var item = line.match(/^(.*?)[\s]+(.*?)$/);
+            if (item) {
+                var type = item[1];
+                item[2].split(" ").forEach(function(extension) {
+                    lookup[extension] = type;
+                });
             }
-        });
-        sys.log("Mime types loaded");
+        }
     });
+    sys.log("Mime types loaded");
     return {
         type: function(extension) {
             if (lookup[extension]) {
